986353496b4a523f48708bf7affce6c9927192d4
Brea Torres <abreaw328@gmail.com>
2018-01-23 22:01:16 -0500

    models user, events, meetup drafts completed

--- models/users.js
+++ models/users.js
@@ -6,8 +6,10 @@ var Sequelize = require("sequelize");
 // sequelize (lowercase) references our connection to the DB.
 var sequelize = require("../config/connection.js");
 
-// Creates a "User" model that matches up with DB
-var User = sequelize.define("user", {
+console.log("user model here");
+
+// Creates a "Users" model that matches up with DB
+var Users = sequelize.define("user", {
   
   // User email
   user_email: {
@@ -34,8 +36,10 @@ var User = sequelize.define("user", {
   }
 }, { timestamps: true });
 
+console.log("user table here");
+
 // Syncs with DB
-User.sync();
+Users.sync();
 
 // Makes the User Model available for other files (will also create a table)
-module.exports = User;
+module.exports = Users;
