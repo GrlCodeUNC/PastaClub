4f18b645261877ab584ec21525da491a6c57cdae
Anisha Patel <patella08@yahoo.com>
2018-01-25 19:14:55 -0500

    dashboard.js

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-      $('.parallax').parallax();
-    });
+$.get("/login", function(data){
+  console.log(data);
+  $("#").empty();
+
+  if(!data) {
+    $("#").append("<p> Your Plate </p>");
+  }
+  else {
+    $("#").append("<p>" + data.name + "</p>");
+  } 
+});
