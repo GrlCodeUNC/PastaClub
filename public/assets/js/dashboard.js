83735e96d8c6e03a9ff1270f93fa93d0ef4c671d
Miriam Weirich <weirich.mir@gmail.com>
2018-01-27 11:36:58 -0500

    dash images center

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
       $('.parallax').parallax();
     });
+
+// For each event need to have a event id -- div or data toggle GET 
+
+// modal button will have POST
+
+// 
+
