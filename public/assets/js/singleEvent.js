a6051ba6768979e36c7bd550711b0bd3adc81216
Saima Zia <snz1210@gmail.com>
2018-01-29 09:43:43 -0500

    updates

--- public/assets/js/singleEvent.js
+++ public/assets/js/singleEvent.js
@@ -1,33 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.modal-trigger').leanModal();
 
-});
+    // when user clicks rsvp btn
+    $("#rsvp").on("click", function(event) {
+        event.preventDefault();
 
-// when user clicks rsvp btn
-$("#rsvp").on("click", function(event) {
-    event.preventDefault();
+        //make a rsvp obj
+        var newRsvp = {
+            // Title from eventName input
+            item: $("#bringing").val().trim(),
+            // Month from month input
+            comment: $("#comments").val().trim(),
 
-    //make a rsvp obj
-    var newRsvp = {
-        // Title from eventName input
-        Items: $("#bringing").val().trim(),
-        // Month from month input
-        Comments: $("#comments").val().trim(),
-    };
-    
-    $.post("/api/newRsvp", newRsvp)
-    // on success, run this callback
-    .then(function(data) {
-      // log the data we found
-      console.log(data);
-      // tell the user we're adding a character with an alert window
-      alert("Adding new rsvp...");
-    });
+            userId: 1,
 
-      $("#rsvp").val("");
+            eventId: 5,
+
+        };
+
+        $.post("/api/RSVP", newRsvp)
+            // on success, run this callback
+            .then(function(data) {
+                // log the data we found
+                console.log(data);
+                // tell the user we're adding a character with an alert window
+                alert("Adding new rsvp...");
+            });
+
+        $("#rsvp").val("");
         $("#bringing").val("");
         $("#comments").val("");
+    });
+
+
 });
-
-
-// send an AJAX POST-request with jQuery
