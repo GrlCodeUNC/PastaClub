8be8dd69db043e3e79f3db21faba6af330e9c61b
Miriam Weirich <weirich.mir@gmail.com>
2018-01-29 17:54:33 -0500

    dash modal troubles

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,125 +1,97 @@
-// get the id to be able to add it to the dashboard/get function below
-// document 
 
-$(document).ready(function(){
-  $('.modal-trigger').leanModal();
-});
+ $(document).ready(function(){
 
-// $(document).ready(function(){
-//              if (jQuery) {  
-//                // jQuery is loaded  
-//                alert("Yeah!");
-//              } else {
-//                // jQuery is not loaded
-//                alert("Doesn't Work");
-//              }
-//           });
-// $(document).ready(function(){
-//   $('.modal-trigger').leanModal();
-// });
+   $('.modal-trigger').leanModal();
 
-// $(document).ready(function(){
-//              if (jQuery) {  
-//                // jQuery is loaded  
-//                alert("Yeah!");
-//              } else {
-//                // jQuery is not loaded
-//                alert("Doesn't Work");
-//              }
-//           });
+   var id = "tokenID 1";
 
+   $.get("api/dashboard/" + id, function(data) {
 
-$(document).ready(function(){
+   	 console.log(data);
 
-      $('.modal-trigger').leanModal();
+   var name = date.user_name.split(" "),
+   		firstName = fullName[0];
 
- //      $.get("/api/dashboard/" + userID, function(data){
+   		var userID = data.id;
 
- //  	  	console.log(data);
+   	$("#plate").empty();
 
-	//   	var name = data.user_name.split(" "),
-	//       	firstName = fullName[0];
+   	if(!data) {
+   		$("#plate").append("<p> Your Plate </p>");
+   		$("#hostingDash").append("<p> Sorry for our mess")
+   	}
+   		else{
+   			$("#plate").append("<p>" + firstName + "'s Plate</p>");
+   		}
+   });
 
- //  	  	var userID = data.id;
+   var hosting = "data.events_title";
 
- //  	  $("#plate").empty();
+   var id = "tokenID 1";
 
-	//   if(!data) {
-	//     $("#plate").append("<p> Your Plate </p>");
-	//     $("#hostingDash").append("<p> Sorry for our mess")
-	//   }
- //  	  else {
- //    	$("#plate").append("<p>" + firstName + "'s Plate</p>");
- //  	  } 
-	// });
+   		$.get("/api/dashboard/" + id, function(data) {
+
+   			console.log(data);
+
+   			var hosting = "data.events_title";
+
+   			$("#host").empty();
+   		});
 
 
-
-  var hosting = data.events_title;
-
-	var id = "tokenID 1";
+ });
 
 
-    	$.get("/api/dashboard/" + id, function(data){
-
-      		console.log(data);
-
-      		var hosting = data.events_title;
-
-      			$("#host").empty();
-
-	});
-});
-
-// For each event need to have a event id -- div or data toggle GET 
-
-// modal button will have POST
-
-// 
-
-//when user clicks createEvent btn
-
-// $("#createEvent").on("click", function(event) {
+// this disables the modal....??/
+//  $("#createEvent").on("click", function(event) {
 //     event.preventDefault();
 
-    //make a newEvent obj
-    // var newEvent = {
-        // Title from eventName input
-        // Title: $("#eventName").val().trim(),
-        // Month from month input
-        // Month: $("#month").val().trim(),
-        // Day from day input
-        // Day: $("#day").val().trim(),
-        // Host from host input
-        // Host: $("#hostName").val().trim(),
-        // Start from startTime input
-        // Start: $("startTime").val().trim(),
-        // End from endTime input
-        // End: $("endTime").val().trim(),
-        // Location from location input
-        // Location: $("location").val().trim(),
     
+//     var newEvent = { 
+//         Title: $("#eventName").val().trim(),    
+//         Month: $("#month").val().trim(),
+//         Day: $("#day").val().trim(),
+//         Host: $("#hostName").val().trim(),
+//         Start: $("startTime").val().trim(),
+//         End: $("endTime").val().trim(),
+//         Street: $("street").val().trim(),
+//         State: $("state").val().trim(),
+//         Zip: $("zip").val().trim(),
+         
 // }; 
 
+$.post("/api/newEvent", newEvent)
+	.then(function(data) {
+		console.log(data);
 
-// send an AJAX POST-request with jQuery
-  // $.post("/api/newEvent", newEvent)
-    // on success, run this callback
-    // .then(function(data) {
-      // log the data we found
-      // console.log(data);
-      // tell the user we're adding a character with an alert window
-    //   alert("Adding new event...");
-    // });
+		alert("Adding new event...");
+	});
 
+// this disables the modal....??/	
 
-
-//       $("#eventName").val("");
-//         $("#month").val("");
-//         $("#day").val("");
-//         $("#hostName").val("");
-//         $("#startTime").val("");
-//         $("#endTime").val("");
-
+// 	   $("#eventName").val("");
+//     $("#month").val("");
+//     $("#day").val("");
+//     $("#hostName").val("");
+//     $("#startTime").val("");
+//     $("#endTime").val("");
+//     $("#street").val("");
+//     $("#state").val("");
+//     $("#zip").val("");
 
 // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
